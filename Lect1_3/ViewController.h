50a6207f9d49bef08c561224f94856bb697bb703
azzaz <azzaz@azzazs-Mac.local>
2018-03-07 11:56:35 +0200

    Initial Commit

---
+++ Lect1_3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lect1_3
+//
+//  Created by azzaz on 3/7/18.
+//  Copyright © 2018 azzaz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ Lect1_3/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Lect1_3
+//
+//  Created by azzaz on 3/7/18.
+//  Copyright © 2018 azzaz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
